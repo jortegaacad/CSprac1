f37335a6f809e2b0797dbaf271322fa08e35a824
jortegaacad <154738263+jortegaacad@users.noreply.github.com>
2023-12-26 22:49:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
-import random
+import git
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   r = lambda: random.randint(0,255)
-   return render_template('index.html', bgcolor=f"rgb({r()},{r()},{r()})")
+   repo = git.Repo(os.getcwd(), search_parent_directories=True)
+   sha = repo.head.object.hexsha
+   r = int(sha[0:2], 16)
+   g = int(sha[2:4], 16)
+   b = int(sha[4:6], 16)
+   return render_template('index.html', bgcolor=f"rgb({r},{g},{b})")
 
 if __name__ == '__main__':
    app.run(debug=True)
